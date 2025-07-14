0d959c1919a8e277541e295d4425287e7527c8ee
JeyZ9 <ggjj4511@gmail.com>
2025-07-14 17:52:00 +0700

    api create restaurant

--- server/index.js
+++ server/index.js
@@ -1,19 +1,23 @@
 // const express = require('express');
-import express, { urlencoded } from "express";
+import express from "express";
 // const dotenv = require("dotenv")
 import dotenv from "dotenv";
-dotenv.config()
+import restaurantRouter from "./routers/restaurant.router.js";
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Hello Nodemon 555')
-})
+  res.send('Hello Nodemon 555');
+});
+
+// use router
+app.use("/api/v1/restaurants", restaurantRouter);
 
 app.listen(PORT, () => {
   console.log("Listening to http://localhost:" + PORT);
-})
+});
